b9088ce681898ce58ecee2125ce8a2271d38bf95
jaj <jona@joachim.cc>
2018-05-22 15:04:21 +0200

    Add ZeroMQ writer

---
+++ src/ZeromqWriter.kt
@@ -0,0 +1,32 @@
+import common.DeviceIdentity
+import datatypes.Data
+import datatypes.SampleArray
+import export.serializers.Serializer
+import export.writers.Writer
+
+import org.zeromq.ZContext
+import org.zeromq.ZMQ
+
+class ZeromqWriter(serializer: Serializer, zmqdest: String = "127.0.0.1") : Writer(serializer) {
+    val context = ZContext()
+    val waveSocket = context.createSocket(ZMQ.PUB)
+    val numSocket = context.createSocket(ZMQ.PUB)
+
+    init {
+        numSocket.connect("tcp://$zmqdest:4201")
+        waveSocket.connect("tcp://$zmqdest:4202")
+    }
+
+    private fun prependTopic(bytes: ByteArray) : ByteArray{
+        val topic = "PHI ".toByteArray(ZMQ.CHARSET)
+        return topic + bytes
+    }
+    override fun write(deviceIdentity: DeviceIdentity?, data: Data?) {
+        if (data == null)
+            return
+        val destSocket = if (data is SampleArray) waveSocket else numSocket
+        val msgList = this.serializer.serializeToBytes(deviceIdentity, data)
+        for (msg in msgList)
+            destSocket.send(prependTopic(msg))
+    }
+}
